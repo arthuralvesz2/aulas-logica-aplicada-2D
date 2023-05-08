ddc8e5bd4c29ca2367abe371257ba38a1b1ceb9a
arthuralvesz2 <arthur_alves.2007@hotmail.com>
2023-05-08 11:51:23 -0300

    Tudo foi apagado.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Laço enquanto - while
-        int contador = 0;
-        while(contador < 10){
-            // Processos
-            System.out.println("Contador = " + contador);
-            contador = contador + 1; // Incremento
-        }
+        // Aula inicial de Orientação à Objetos
 
-        // Laço faça enquanto - do while
-        int cont = 1;
-        do{
-            System.out.println(cont);
-            cont++; // Incremento
-        }while(cont <= 10);
-
-        // Laço para faça por
-        for(int x=0; x<10; x++){
-            System.out.println(x);
-        }
     }
 }
